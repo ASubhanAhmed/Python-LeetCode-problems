65b446a76708da378282a4d93d1be02caffc616a
ASubhanAhmed <03furqanansari@gmail.com>
2022-08-02 20:22:24 +0500

    *yt* Turtal Spirograph

---
+++ Turtal Spirograph python Subhan.py
@@ -0,0 +1,18 @@
+'''
+https://www.youtube.com/watch?v=w0LO0Ff7WRo
+    TURTAL 'SPIROGRAPH'
+'''
+
+import turtle
+
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(0)
+
+for i in range(6):
+    for colours in ["red", "magenta", "blue", "cyan", "green", "yellow", "white"]:
+        turtle.color(colours)
+        turtle.circle(100)
+        turtle.left(10)
+
+turtle.hideturtle()
